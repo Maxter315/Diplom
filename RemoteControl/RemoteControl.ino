12215d3f7cee2c4eabe40f338b7c59e9c2007877
Maxter315 <maxter315@gmail.com>
2017-03-28 12:01:56 +0300

    upd

--- RemoteControl/RemoteControl.ino
+++ RemoteControl/RemoteControl.ino
@@ -5,15 +5,14 @@
 #include <stdio.h>
 
 #include "Screen_HX8353E.h"
-Screen_HX8353E myScreen;
 
 #include "uprot.h"
 #include "def.h"
 
+Screen_HX8353E mkScreen;
 Enrf24 radio(CSN, CE, IRQ);
 const uint8_t txaddr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0x01 };
 const uint8_t rxaddr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0x03 };
-
 DataCtrl codogr;
 
 void dump_radio_status_to_serialport(uint8_t);
@@ -21,103 +20,111 @@ void spiSwitch(uint8_t dev);
 
 /*============================================================================*/
 void setup() {
-    pinMode(JOY_SEL, INPUT_PULLUP);
-    analogReadResolution(12);
+    /* EDU_MK2 Joystick setup */
+        pinMode(JOY_SEL, INPUT_PULLUP);
+        analogReadResolution(12);
+        pinMode(KEY_A, INPUT_PULLUP);
+        pinMode(KEY_B, INPUT_PULLUP);
 
-    pinMode(KEY_A,INPUT_PULLUP);
-    pinMode(KEY_B,INPUT_PULLUP);
+    /* Communication initialization */
+        Serial.begin(9600);
 
-  Serial.begin(9600);
+        SPI.begin();
+        SPI.setDataMode(SPI_MODE0);
+        SPI.setBitOrder(MSBFIRST);
 
-  SPI.begin();
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setBitOrder(MSBFIRST);
+    /* EDU_MK2 LCD setup */
+        mkScreen.begin();
+        mkScreen.setOrientation(0);
+        mkScreen.clear(blackColour);
+        mkScreen.setPenSolid(false);
+        mkScreen.gText(48, 48, "(FUG)");
 
-  myScreen.begin();
-  myScreen.setOrientation(0);
-  myScreen.clear(blackColour);
-  myScreen.setPenSolid(false);
-  myScreen.gText(48, 48, "(FUG)");
-
-  radio.begin();  // Defaults 1Mbps, channel 0, max TX power
-  dump_radio_status_to_serialport(radio.radioState());
-
-  radio.setTXaddress((void*)txaddr);
-  radio.setRXaddress((void*)rxaddr);
-  
+    /* nRF24l01+ setup */
+        radio.begin();  // Defaults 1Mbps, channel 0, max TX power
+        dump_radio_status_to_serialport(radio.radioState());
+        radio.setTXaddress((void*)txaddr);
+        radio.setRXaddress((void*)rxaddr);
 }
 
 int16_t xx, yy;
 int16_t ax, ay, az;
-        char inbuf[33];
-        DataResp* ptr_cdg;
+float mainBattery;
+
+char inbuf[33];
+DataResp* ptr_cdg;
 
 void loop() { 
 
-    spiSwitch(2);
-    
-    xx = analogRead(JOY_X) - 2048;
-    yy = analogRead(JOY_Y) - 2048;
-    
-    ax = analogRead(ACC_X) - 2048;
-    ay = analogRead(ACC_Y) - 2048;
-    az = analogRead(ACC_Z) - 2048;
-    
-    codogr.setJoy(xx, yy, !(bool)digitalRead(JOY_SEL));
-    codogr.setKeys(!(bool)digitalRead(KEY_A), !(bool)digitalRead(KEY_B));
-    codogr.setAcc(ax, ay, az);
+    /* Reading Joystick position and orientation */
+        xx = analogRead(JOY_X) - 2048;
+        yy = analogRead(JOY_Y) - 2048;
 
-    char val[10];
+        ax = analogRead(ACC_X) - 2048;
+        ay = analogRead(ACC_Y) - 2048;
+        az = analogRead(ACC_Z) - 2048;
 
-    //myScreen.rectangle(0,0,128,32,blackColour);
-    sprintf(val,"jx=%6.1d",xx);    myScreen.gText(0, 0, val);
-    sprintf(val,"jy=%6.1d",yy);    myScreen.gText(0, 8, val);
-    sprintf(val,"ax=%6.1d",ax);    myScreen.gText(64, 0, val);
-    sprintf(val,"ay=%6.1d",ay);    myScreen.gText(64, 8, val);
-    sprintf(val,"az=%6.1d",az);    myScreen.gText(64, 16, val);
-    myScreen.gText(0, 16, " ");
+        // Forming data to transmitt 
+        codogr.setJoy(xx, yy, !(bool)digitalRead(JOY_SEL));
+        codogr.setKeys(!(bool)digitalRead(KEY_A), !(bool)digitalRead(KEY_B));
+        codogr.setAcc(ax, ay, az);
 
-    spiSwitch(1);
-  //radio.print(str_off);
-    radio.write(&codogr,16);
-    radio.flush();  //
-    radio.enableRX();
-        int cnt;
+    /* Switching SPI to LCD */
+        spiSwitch(2);
 
-        cnt = 5000;
+        // Displaying all the information
+        char val[10];
+        //mkScreen.rectangle(0,0,128,32,blackColour);
+        sprintf(val,"jx=%6.1d",xx);    mkScreen.gText(0, 0, val);
+        sprintf(val,"jy=%6.1d",yy);    mkScreen.gText(0, 8, val);
+        sprintf(val,"ax=%6.1d",ax);    mkScreen.gText(64, 0, val);
+        sprintf(val,"ay=%6.1d",ay);    mkScreen.gText(64, 8, val);
+        sprintf(val,"az=%6.1d",az);    mkScreen.gText(64, 16, val);
+        mkScreen.gText(0, 16, " ");
+
+    /* Switching SPI to nRF24l01+ */
+        spiSwitch(1);
+
+        // Sending packet to the Machine
+        radio.write(&codogr,16);
+        radio.flush();
+        radio.enableRX();
         
-        while (!radio.available(false) && cnt) {delayMicroseconds(1); cnt--;}
+        // Awainting for response from the Machine
+        int timeout;
+        timeout = 5000;
+        while (!radio.available(true) && timeout){
+            delayMicroseconds(1);
+            timeout--;
+        }
         if(radio.read(inbuf)){
+            // Response is received
             ptr_cdg = (DataResp*)&inbuf;
             Serial.print("bat: ");
             Serial.println(ptr_cdg->battery);
         }
         radio.disableRX();
         
-    Serial.print("acc \tx: ");
-    Serial.print(ax);
-    Serial.print("\ty: ");
-    Serial.print(ay);
-    Serial.print("\tz: ");
-    Serial.println(az);
-    delay(1000);
+        // debug
+        Serial.print("acc \tx: ");
+        Serial.print(ax);
+        Serial.print("\ty: ");
+        Serial.print(ay);
+        Serial.print("\tz: ");
+        Serial.println(az);
+
+    delay(500);
 }
 /*============================================================================*/
 
 void spiSwitch(uint8_t dev){
     if(dev == 1){
         //nRF24
-        //delay(1);
         digitalWrite(LCD_CS,HIGH);    pinMode(LCD_CS,OUTPUT);
-        //delay(1);
         digitalWrite(CSN,LOW);       pinMode(CSN,OUTPUT);
-        //delay(1);    
     }else if(dev == 2){
-        //delay(1);
         digitalWrite(CSN,HIGH);       pinMode(CSN,OUTPUT);
-        //delay(1);
         digitalWrite(LCD_CS,LOW);    pinMode(LCD_CS,OUTPUT);
-        //delay(1);
     }
 }
 
--- TheMachine/TheMachine.ino
+++ TheMachine/TheMachine.ino
@@ -6,69 +6,114 @@
 #include "uprot.h"
 #include "machinedef.h"
 
+#include <AFMotor.h>
 #include <Servo.h>
 
 Enrf24 radio(CSN, CE, IRQ);
 const uint8_t rxaddr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0x01 };
 const uint8_t txaddr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0x03 };
 
+Servo steerServ;
+int steerPos;
+AF_DCMotor engine(4);
+int engPower;
+
 void dump_radio_status_to_serialport(uint8_t);
 
 /*============================================================================*/
 void setup() {
-  Serial.begin(9600);
+    /* Hardware setup */
+        steerServ.attach(SERVO1,600,2400);
+        steerServ.write(90);
+        engine.run(RELEASE);
 
-  SPI.begin();
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setBitOrder(MSBFIRST);
-  
-  radio.begin();  // Defaults 1Mbps, channel 0, max TX power
-  dump_radio_status_to_serialport(radio.radioState());
+    /* Communication initialization */
+        Serial.begin(9600);
 
-  radio.setRXaddress((void*)rxaddr);
-  radio.setTXaddress((void*)txaddr);
-  
-  radio.enableRX();  // Start listening
+        SPI.begin();
+        SPI.setDataMode(SPI_MODE0);
+        SPI.setBitOrder(MSBFIRST);
+
+    /* nRF24l01+ setup */
+        radio.begin();  // Defaults 1Mbps, channel 0, max TX power
+        dump_radio_status_to_serialport(radio.radioState());
+        radio.setRXaddress((void*)rxaddr);
+        radio.setTXaddress((void*)txaddr);
+        radio.enableRX();  // Start listening
 }
 
+char inbuf[33];
+DataCtrl* ptr_cdg;
+DataResp response;
 
 void loop() {
-    char inbuf[33];
-    DataCtrl* ptr_cdg;
-    DataResp response; 
-
-radio.enableRX();
-    while (!radio.available(true));
-    if (radio.read(inbuf)) {
-    response.battery = 120;
-    radio.disableRX();
     
-    radio.write(&response,8);
-    radio.flush();
+    /* Listening channel for control */
+        int timeout;
+        timeout = 50000;
+        radio.enableRX();
+        while (!radio.available(true)){
+            delayMicroseconds(1);
+            timeout--;
+            if(!timeout){
+                timeout = 50000;
+                engPower = 0;
+                motor.run(RELEASE);
+            }
+        }
+        if (radio.read(inbuf)) {
+            // Packet received
+            radio.disableRX();
+            ptr_cdg = (DataCtrl*)&inbuf;
 
-    Serial.print("Received packet: ");
-    Serial.println(1);
+            // Preparing response
+            float bat;
+            bat = 2 * analogRead(BAT_ADC) * (5.0/1024.0);
+            response.battery = bat;
+            response.servo_a = steerServ;
 
-    ptr_cdg = (DataCtrl*)&inbuf;
-    Serial.print("joy\tx: ");
-    Serial.print(ptr_cdg->joy_x);
-    Serial.print("\ty: ");
-    Serial.print(ptr_cdg->joy_y);
-    Serial.print("\tsel: ");
-    Serial.print(ptr_cdg->joy_sel);
-    
-    Serial.print("\tk_a: ");
-    Serial.print(ptr_cdg->key_a);
-    Serial.print("\tk_b: ");
-    Serial.println(ptr_cdg->key_b);
+            // Sending response
+            radio.write(&response,8);
+            radio.flush();
 
-    Serial.print("acc \tx: ");
-    Serial.print((ptr_cdg->acc_x));
-    Serial.print("\ty: ");
-    Serial.print((ptr_cdg->acc_y));
-    Serial.print("\tz: ");
-    Serial.println((ptr_cdg->acc_z));
-  }
+            /* Controlling motors and servos */
+                steerPos += ptr_cdg->joy_x;
+                engPower = ptr_cdg->joy_y;
+                if(engPower > 10){
+                    motor.run(FORWARD);
+                    motor.setSpeed(map(abs(engPower),0,2048,0,255));
+                }else if(engPower > -10){
+                    motor.run(BACKWARD);
+                    motor.setSpeed(map(abs(engPower),0,2048,0,255));
+                }else{
+                    motor.run(RELEASE);
+                }
+                
+            // debug
+            Serial.print("Received packet: ");
+            Serial.println(1);
+
+            Serial.print("joy\tx: ");
+            Serial.print(ptr_cdg->joy_x);
+            Serial.print("\ty: ");
+            Serial.print(ptr_cdg->joy_y);
+            Serial.print("\tsel: ");
+            Serial.print(ptr_cdg->joy_sel);
+            
+            Serial.print("\tk_a: ");
+            Serial.print(ptr_cdg->key_a);
+            Serial.print("\tk_b: ");
+            Serial.println(ptr_cdg->key_b);
+
+            Serial.print("acc \tx: ");
+            Serial.print((ptr_cdg->acc_x));
+            Serial.print("\ty: ");
+            Serial.print((ptr_cdg->acc_y));
+            Serial.print("\tz: ");
+            Serial.println((ptr_cdg->acc_z));
+        }
+
+
 }
 
 /*============================================================================*/
